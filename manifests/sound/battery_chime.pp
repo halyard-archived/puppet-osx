1b7cb689076121305d4332ff4e5bb8b1cb0ab959
Les Aker <me@lesaker.org>
2015-11-03 19:09:27 -0500

    fix battery chime setting

--- manifests/sound/battery_chime.pp
+++ manifests/sound/battery_chime.pp
@@ -3,15 +3,10 @@ class osx::sound::battery_chime (
   $enable = false
 ) {
 
-  $value = $enable ? {
-    false   => 0,
-    default => 1
-  }
-
   osx_default { 'Enable feedback on charging cable connection':
     domain => 'com.apple.PowerChime',
     key    => 'ChimeOnAllHardware',
-    value  => $value,
+    value  => $enable,
     type   => 'boolean'
   }
 }
