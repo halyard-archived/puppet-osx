5168b2c60c4c3fdd75f6088caadb9281de9d71c0
Les Aker <me@lesaker.org>
2016-01-24 12:52:57 -0500

    fix keyboard dim default

--- manifests/keyboard/dim_backlight.pp
+++ manifests/keyboard/dim_backlight.pp
@@ -1,6 +1,6 @@
 # Public: Dim backlight after inactivity (Delay set in seconds)
 class osx::keyboard::dim_backlight(
-  $delay = 60
+  $delay = 10
 ) {
   boxen::osx_defaults { "Dim keyboard after ${delay} seconds of inactivity":
     user   => $::boxen_user,
